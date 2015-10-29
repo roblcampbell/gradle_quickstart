be69a9ce9de8e49b83c55649d79aa653e4ebfc56
roblcampbell <rcampbell@datarecognitioncorp.com>
2015-10-29 15:10:48 -0500

    first commit

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,11 @@
+package hello;
+//import org.apache.commons.lang3.CharUtils;
+
+public class Hello {
+
+public static void main(String[] args) {
+	System.out.println("Hello From Java!");
+	//System.out.println(CharUtils.toChar("R", DEFAULT_INPUT));
+	
+}
+}
